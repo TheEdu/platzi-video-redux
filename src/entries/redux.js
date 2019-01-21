7d57b96433cd0a7c16d41d0384167c16c9dd8a78
Eduardo Denis <edigde@gmail.com>
2019-01-21 11:37:06 -0300

    Entendiendo y agregando un Store

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,3 +1,6 @@
+import { createStore } from 'redux'
+
+
 const handleSubmit = (event) => {
   event.preventDefault();
   const data = new FormData($form);
@@ -7,3 +10,23 @@ const handleSubmit = (event) => {
 
 const $form = document.getElementById('form');
 $form.addEventListener('submit', handleSubmit);
+
+const initialState = [
+  {
+    "title": "Cancion 1"
+  },
+  {
+    "title": "Cancion 2"
+  },
+  {
+    "title": "Cancion 3"
+  },
+]
+
+const store = createStore(
+  (state) => {
+    return state
+  }, // reducer: función pura que retorna el próximo estado
+  initialState, //  PreloadState / InitialState = Estado inicial de la aplicación, primere carga, llamado al api. Puede ser cualquier tipo de dato
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Enhancer = función que puede extender redux con capacidades añadidas por librerías externas. Es un parámetro opcional.
+)
